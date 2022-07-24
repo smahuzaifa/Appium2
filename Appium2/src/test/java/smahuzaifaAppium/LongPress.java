a988bb2c2d235faed82fc0b06ba1d68f03c47ba3
smahuzaifa <huzaifasadath@gmail.com>
2022-07-24 15:39:01 +0530

    Added more comments to long press
    Included link for android-mobile-gestures and also the code and its
    attributes text

--- Appium2/src/test/java/smahuzaifaAppium/LongPress.java
+++ Appium2/src/test/java/smahuzaifaAppium/LongPress.java
@@ -18,7 +18,22 @@ public class LongPress extends BaseTest{
 		 driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"Expandable Lists\"]")).click();
 		 driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"1. Custom Adapter\"]")).click();
 		 
-		 //Appium github gestures
+		 /* Appium github gestures
+		 *  https://appium.io/docs/en/writing-running-appium/android/android-mobile-gestures/
+		 * 
+		 * mobile: longClickGesture
+			This gesture performs long click action on the given element/coordinates. Available since Appium v1.19
+			
+			Supported arguments:		
+			elementId: The id of the element to be clicked. If the element is missing then both click offset coordinates must be provided. If both the element id and offset are provided then the coordinates are parsed as relative offsets from the top left corner of the element.
+			x: The x-offset coordinate
+			y: The y-offset coordinate
+			duration: Click duration in milliseconds. 500 by default. The value must not be negative
+			
+			Code:
+			((JavascriptExecutor) driver).executeScript("mobile: longClickGesture", ImmutableMap.of(
+    		"elementId", ((RemoteWebElement) element).getId()));
+		 */
 		 WebElement ele = driver.findElement(By.xpath("//android.widget.TextView[@text='People Names']"));
 		 ((JavascriptExecutor)driver).executeScript("mobile: longClickGesture", 
 				 ImmutableMap.of("elementId",((RemoteWebElement)ele).getId(),
