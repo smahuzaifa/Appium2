86b7c544c45de587e0dc3d739bc86f3bcbb9adb7
smahuzaifa <smahuzaifa@localhost>
2022-07-31 18:39:43 +0530

    Verify Toast

--- Appium2/src/test/java/generalStore/VerifyToast.java
+++ Appium2/src/test/java/generalStore/VerifyToast.java
@@ -1,21 +1,22 @@
 package generalStore;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 
-public class FillForm extends BasicTest{
+public class VerifyToast extends BasicTest{
 	@Test
-	public void fillForm() throws InterruptedException {
-		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("ABCD");
-		driver.hideKeyboard();
-		driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
-		driver.findElement(By.id("android:id/text1")).click(); //Country selector
+	public void verifyToastMessaeg() {
+		driver.findElement(By.id("android:id/text1")).click();
 		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"))"));
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		Thread.sleep(3000);
+		//Toast message tag name: android.widget.Toast
+		String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+		System.out.println(toastMessage);
+		Assert.assertEquals(toastMessage,"Please enter your name");
 	}
 
 }
