5f686f74adfd37d8937e72c2b152575c3a787a5a
smahuzaifa <smahuzaifa@localhost>
2022-07-31 13:41:24 +0530

    Added miscellaneous actions
    1. Copy paste from clip board
    2. Using device button like back, enter, home

--- Appium2/src/test/java/smahuzaifaAppium/MiscellaneousAppiumActions.java
+++ Appium2/src/test/java/smahuzaifaAppium/MiscellaneousAppiumActions.java
@@ -11,6 +11,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 //import io.appium.java_client.android.AndroidDriver;
 //import io.appium.java_client.android.options.UiAutomator2Options;
 //import io.appium.java_client.remote.MobileCapabilityType;
@@ -39,10 +41,19 @@ public class MiscellaneousAppiumActions extends BaseTest {
 		String alerttitle= driver.findElement(By.id("android:id/alertTitle")).getText();
 		System.out.println(alerttitle);
 		Assert.assertEquals(alerttitle, "WiFi settings");
+		
+		//copy paste
+		//copy to clipboard - paste it to clipboard
+		driver.setClipboardText("ABCD");
+	
 		//This code will compare app that has got from app and the actual
-		driver.findElement(By.id("android:id/edit")).sendKeys("ABCD");
+		driver.findElement(By.id("android:id/edit")).sendKeys(driver.getClipboardText());
+		//Android Keys
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
 		//driver.findElement(By.id("android:id/button1")).click();
 		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
+		driver.pressKey(new KeyEvent(AndroidKey.BACK)); //This will press Back button of mobile
+		driver.pressKey(new KeyEvent(AndroidKey.HOME)); //This will press Home Button of mobile
 		Thread.sleep(4000);
 		//Can be used when only class name is present and multiple instances of same class is present
 		//tearDown(); //Similar to Before class we are using AfterClass here
